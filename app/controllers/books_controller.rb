9052891436864305921ffc0eaf170d8dac4d2f63
makiharu <kitakazetoshirokuma@gmail.com>
2020-05-19 13:03:18 +0000

    name_search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,53 +1,69 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
+#  before_action :baria_user, only: [:edit, :update, :destroy]
+  before_action :ensure_correct_user,only: [:edit,:update,:destroy]
+  def show
+    @book = Book.new
+    @onebook = Book.find(params[:id])
+    @user = User.find(@onebook.user_id)
+  end
 
   def index
-  	@books = Book.all
-  	@book = Book.new
+    @books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+    @book = Book.new
   end
 
   def create
-  	@book = Book.new(book_params)
-  	@book.user_id = current_user.id
-  	if @book.save
-  		redirect_to book_path(@book.id), notice: "success" #redirect_to @bookと同義
-  	else
-  		@books = Book.all
-  		render 'index'
-  	end
-  end
-
-  def show
-  	@onebook = Book.new
-  	@book = Book.find(params[:id]) #追記
-  	@user = @book.user
-
+    @books = Book.all
+    @book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+    @book.user_id = current_user.id
+    if @book.save #入力されたデータをdbに保存する。
+      redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
+    else
+      @books = Book.all
+      render 'index'
+    end
   end
 
   def edit
-  	@book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
+
+
   def update
-  	@book = Book.find(params[:id])
-  	@book.user_id = current_user.id
-  	if @book.update(book_params)
-  		redirect_to @book, notice: "success"
-  	else
-  		render 'edit'
-  	end
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      redirect_to @book, notice: "successfully updated book!"
+    else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destoy
+    redirect_to books_path, notice: "successfully delete book!"
   end
 
   private
+
   def book_params
-  	params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 
-  def ensure_correct_user
-  	@book = Book.find(params[:id])
-  	if @book.user_id != current_user.id
-  		redirect_to books_path
-  	end
-  end
+
+      def ensure_correct_user
+      @book = Book.find(params[:id])
+      if @book.user_id != current_user.id
+        redirect_to books_path
+      end
+    end
+
+
 
 end
+
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,38 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable,:validatable
 
-  has_many :books
+  has_many :books, dependent: :destroy
+  attachment :profile_image, destroy: false
 
-  attachment :profile_image
+   validates :name, presence: true, length: {maximum: 20, minimum: 2}
+   validates :introduction, length: { maximum: 50}
 
-  validates :name, presence: true
-  validates :introduction, length: {maximum: 50}
+
+ # def self.search(search) #self.でクラスメソッドとしている
+  #  if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
+   #   User.where(['name LIKE ?', "%#{search}%"])
+    #else
+     # User.all #全て表示。
+    #end
+  #end
+#end
+
+
+    def self.search(search)   
+        if search  
+          where(['name LIKE ?', "%#{search}%"])   
+        else  
+          all  
+        end  
+    end  
+
 end
+
+#LIKE句はSQLの検索を行うための演算子
+#name LIKEと書くことでnameカラムを検索、という意味
+
+#?は次の引数"%hana%"を受け取る場所になります。最終的にname LIKE "%hana%"という風になるということです。%は任意の文字列を指します。
+
+#nameカラムにxxxxを含むレコードを抽出する、
