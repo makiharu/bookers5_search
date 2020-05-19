875f47ae406f921819f70426787c5cbf6755be94
makiharu <kitakazetoshirokuma@gmail.com>
2020-05-19 08:08:19 +0000

    viewpage

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,53 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+  def create
+  	@book = Book.new(book_params)
+  	@book.user_id = current_user.id
+  	if @book.save
+  		redirect_to book_path(@book.id), notice: "success" #redirect_to @bookと同義
+  	else
+  		@books = Book.all
+  		render 'index'
+  	end
   end
 
   def show
+  	@onebook = Book.new
+  	@book = Book.find(params[:id]) #追記
+  	@user = @book.user
+
   end
 
   def edit
+  	@book = Book.find(params[:id])
   end
+
+  def update
+  	@book = Book.find(params[:id])
+  	@book.user_id = current_user.id
+  	if @book.update(book_params)
+  		redirect_to @book, notice: "success"
+  	else
+  		render 'edit'
+  	end
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+
+  def ensure_correct_user
+  	@book = Book.find(params[:id])
+  	if @book.user_id != current_user.id
+  		redirect_to books_path
+  	end
+  end
+
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b2a9c963bdae22ae076def6e79e877b85a0beb125e6a1205b7715f1009f9ef18cb55394a5a4612d48377285a616bd16b5688d664ce550de360897d0893db2772'
